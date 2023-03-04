67afe71bd0c28c72fff81f927bd11c145dad7fcc
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2023-03-04 13:39:16 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Route, Routes} from 'react-router';
 import {BrowserRouter} from 'react-router-dom';
-import Footer from './api/components/Footer';
-import Header from './api/components/Header';
-import Home from './api/components/Home';
-import Login from './api/components/login/Login';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/login/Login';
 import './App.css';
 
 const App = () => {
