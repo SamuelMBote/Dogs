4051602f9a921f15fe9abb9950c8d00320d4fc86
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2023-03-08 10:01:32 -0300

    Login Background

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './Login.module.css';
 import {Navigate, Route, Routes} from 'react-router-dom';
 import {UserContext} from '../../UserContext';
 import LoginCreate from './LoginCreate';
@@ -12,14 +13,16 @@ const Login = () => {
   if (login === true) return <Navigate to="/" />;
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={style.login}>
+      <div className={style.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
