23f5ec271e484f2e7542990bd1bc2a3631ba522c
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2023-03-22 13:25:17 -0300

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import style from './Footer.module.css';
+import {ReactComponent as Dogs} from '../Assets/dogs-footer.svg';
 const Footer = () => {
-  return <div className={style.footer}>Footer</div>;
+  return (
+    <div className={style.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados</p>
+    </div>
+  );
 };
 
 export default Footer;
